543cfa1f6e8f9e2cc0d8330faa054779b2a88f6f
Deepak Rai <deepak.rai@inmobi.com>
2015-06-15 11:10:11 +0530

    Audio Control Details1

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/media/AudioControlDetails.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/media/AudioControlDetails.java
@@ -10,6 +10,10 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.media.MediaRouter;
+import android.media.*;
+
+import android.widget.VideoView;
 
 /**
  * Allows clearing SharedPreferences.
@@ -34,14 +38,32 @@ public class AudioControlDetails implements Action {
 		// } catch(Exception e) {
 		// 	return Result.fromThrowable(e);
 		// }
-		AudioManager myAudioManager;
-		
+		AudioManager myAudioManager;		
 		myAudioManager = (AudioManager) InstrumentationBackend.solo.getCurrentActivity().getSystemService(Context.AUDIO_SERVICE);
-		System.out.println("myAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC) " + myAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
-		System.out.println("myAudioManager.toString() " + myAudioManager.toString());
-		System.out.println("myAudioManager.getStreamVolume(AudioManager.STREAM_SYSTEM) " + myAudioManager.getStreamVolume(AudioManager.STREAM_SYSTEM));
+		System.out.println("DRAIAudio myAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC) " + myAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+		System.out.println("DRAIAudio myAudioManager.toString() " + myAudioManager.toString());
+		System.out.println("DRAIAudio myAudioManager.getStreamVolume(AudioManager.STREAM_SYSTEM) " + myAudioManager.getStreamVolume(AudioManager.STREAM_SYSTEM));
+		System.out.println("DRAIAudio myAudioManager.getMode() " + myAudioManager.getMode());
+		System.out.println("DRAIAudio myAudioManager.getProperty(PROPERTY_OUTPUT_SAMPLE_RATE) " + myAudioManager.getProperty("PROPERTY_OUTPUT_SAMPLE_RATE"));
+		System.out.println("DRAIAudio myAudioManager.getProperty(PROPERTY_OUTPUT_FRAMES_PER_BUFFER) " + myAudioManager.getProperty("PROPERTY_OUTPUT_FRAMES_PER_BUFFER"));
 
-
+//		MediaPlayer myMediaPlayer;		
+//		myMediaPlayer = (MediaPlayer) InstrumentationBackend.solo.getCurrentActivity().getSystemService(Context.MEDIA_SESSION_SERVICE);
+		
+		MediaRouter myMediaRouter;
+		myMediaRouter = (MediaRouter) InstrumentationBackend.solo.getCurrentActivity().getSystemService(Context.MEDIA_ROUTER_SERVICE);
+		System.out.println("DRAIAudio myMediaRouter.getCategoryCount " + myMediaRouter.getCategoryCount());
+		System.out.println("DRAIAudio myMediaRouter.getDefaultRoute " + myMediaRouter.getDefaultRoute());
+		System.out.println("DRAIAudio myMediaRouter.getRouteCount " + myMediaRouter.getRouteCount());
+		
+//		MediaSessionManager myMediaSession;
+//		SessionManager = (SessionManager) InstrumentationBackend.solo.getCurrentActivity().getSystemService(Context.MEDIA_SESSION_SERVICE);
+//		Context.MEDIA_ROUTER_SERVICE
+//		MediaController myMediaController;
+		
+		VideoView myVideoView; 
+//		myVideoView = (VideoView) InstrumentationBackend.solo.getCurrentActivity().getSystemService(Context.);
+		
 		return Result.successResult();
 	}
 
