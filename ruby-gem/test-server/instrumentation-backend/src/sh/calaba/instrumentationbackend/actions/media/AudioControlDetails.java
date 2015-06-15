6b3b0f713cfb53e5d1cac53f7eee60c5e9cf553f
Deepak Rai <deepak.rai@inmobi.com>
2015-06-15 13:57:32 +0530

    Audio Control Details1

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/media/AudioControlDetails.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/media/AudioControlDetails.java
@@ -4,16 +4,79 @@ import sh.calaba.instrumentationbackend.InstrumentationBackend;
 import sh.calaba.instrumentationbackend.Result;
 import sh.calaba.instrumentationbackend.actions.Action;
 
+import android.app.Service;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.media.MediaRouter;
 import android.media.*;
+import android.media.session.MediaSessionManager;
+//import android.media.session.SessionManager;
 
 import android.widget.VideoView;
+import android.widget.MediaController;
+
+//import android.content.Context;
+//import android.app.AlarmManager;
+//import android.view.accessibility.AccessibilityManager;
+//import android.accounts.AccountManager;
+//import android.app.ActivityManager;
+//import android.appwidget.AppWidgetManager;
+//import android.app.AppOpsManager;
+//import android.media.AudioManager;
+//import android.os.BatteryManager;
+//import android.bluetooth.BluetoothAdapter;
+//import android.hardware.camera2.CameraManager;
+//import android.view.accessibility.CaptioningManager;
+//import android.content.ClipboardManager;
+//import android.net.ConnectivityManager;
+//import android.hardware.ConsumerIrManager;
+//import android.app.admin.DevicePolicyManager;
+//import android.hardware.display.DisplayManager;
+//import android.app.DownloadManager;
+//import android.os.DropBoxManager;
+//import android.view.inputmethod.InputMethodManager;
+//import android.hardware.input.InputManager;
+//import android.app.job.JobScheduler;
+//import android.app.KeyguardManager;
+//import android.content.pm.LauncherApps;
+//import android.view.LayoutInflater;
+//import android.location.LocationManager;
+//import android.media.projection.MediaProjectionManager;
+//import android.media.MediaRouter;
+//import android.media.session.MediaSessionManager;
+//import android.nfc.NfcManager;
+//import android.app.NotificationManager;
+//import android.net.nsd.NsdManager;
+//import android.os.PowerManager;
+//import android.print.PrintManager;
+//import android.content.RestrictionsManager;
+//import android.app.SearchManager;
+//import android.hardware.SensorManager;
+//import android.os.storage.StorageManager;
+//import android.telecom.TelecomManager;
+//import android.telephony.TelephonyManager;
+//import android.view.textservice.TextServicesManager;
+//import android.media.tv.TvInputManager;
+//import android.app.UiModeManager;
+//import android.hardware.usb.UsbManager;
+//import android.os.UserManager;
+//import android.os.Vibrator;
+//import android.service.wallpaper.WallpaperService;
+//import android.net.wifi.p2p.WifiP2pManager;
+//import android.net.wifi.WifiManager;
+//import android.view.WindowManager;
+////import kotlin.properties.ReadOnlyProperty;
+//import android.app.Fragment;
+//import android.support.v4.app.Fragment as SupportFragment;
+
+
+
 
 /**
  * Allows clearing SharedPreferences.
@@ -46,6 +109,7 @@ public class AudioControlDetails implements Action {
 		System.out.println("DRAIAudio myAudioManager.getMode() " + myAudioManager.getMode());
 		System.out.println("DRAIAudio myAudioManager.getProperty(PROPERTY_OUTPUT_SAMPLE_RATE) " + myAudioManager.getProperty("PROPERTY_OUTPUT_SAMPLE_RATE"));
 		System.out.println("DRAIAudio myAudioManager.getProperty(PROPERTY_OUTPUT_FRAMES_PER_BUFFER) " + myAudioManager.getProperty("PROPERTY_OUTPUT_FRAMES_PER_BUFFER"));
+		System.out.println("DRAIAudio myAudioManager.getParameters(AUDIO_SERVICE) " + myAudioManager.getParameters(Context.AUDIO_SERVICE));
 
 //		MediaPlayer myMediaPlayer;		
 //		myMediaPlayer = (MediaPlayer) InstrumentationBackend.solo.getCurrentActivity().getSystemService(Context.MEDIA_SESSION_SERVICE);
@@ -63,6 +127,19 @@ public class AudioControlDetails implements Action {
 		
 		VideoView myVideoView; 
 //		myVideoView = (VideoView) InstrumentationBackend.solo.getCurrentActivity().getSystemService(Context.);
+		
+		
+		MediaPlayer myMediaPlayer;
+//		MediaController mMedia = new MediaController(this);
+//		mMedia.ge
+		MediaSessionManager myMediaSessionManager;
+		myMediaSessionManager = (MediaSessionManager) InstrumentationBackend.solo.getCurrentActivity().getSystemService(Context.MEDIA_SESSION_SERVICE);
+		System.out.println("DRAIAudio myMediaSessionManager " + myMediaSessionManager);
+
+
+//		List<MediaController> myMediaControllerList =  
+		
+		
 		
 		return Result.successResult();
 	}
