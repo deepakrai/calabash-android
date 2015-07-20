6db1d124074f45a780c596352689406ef232fdb2
Deepak Rai <deepak.rai@inmobi.com>
2015-07-20 10:43:56 +0530

    Bumping the version

--- ruby-gem/lib/calabash-android/version.rb
+++ ruby-gem/lib/calabash-android/version.rb
@@ -1,9 +1,5 @@
 module Calabash
   module Android
-<<<<<<< HEAD
-    VERSION = "0.5.11.pre1"
-=======
-    VERSION = "0.5.12"
->>>>>>> 18cae01e0841eeba5ebbb335adb8900f231819e2
+    VERSION = "0.5.12.pre1"
   end
 end
